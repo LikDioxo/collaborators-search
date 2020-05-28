605b4b57cd08b2eb187d95c221867c8e7bfe940e
LikDioxo <likdioxo@gmail.com>
2020-05-28 17:02:41 +0300

    change files location and README.md

--- coauthors_search/images.py
+++ coauthors_search/images.py
@@ -6,23 +6,23 @@ from coauthors_search.structures import Tree
 from coauthors_search.structures import Author, AuthorCredentials
 from coauthors_search.utils import Fetch, validate_configuration
 
-
 _REQUIREMENTS = ["graph_type", "image_path", "graph_path"]
-_DEFAULT_CONFIGS = {"graph_name": "Authors",
-                    "format": "pdf",
-                    'additional_nodes': 0,
-                    'main_branch_color': "BLACK",
-                    'additional_branch_color': "BLACK"
-                    }
+_DEFAULT_CONFIGS = {
+    "graph_name": "Authors",
+    "format": "pdf",
+    'additional_nodes': 0,
+    'main_branch_color': "BLACK",
+    'additional_branch_color': "BLACK"
+}
 
 
 @validate_configuration(_REQUIREMENTS, _DEFAULT_CONFIGS)
-def generate_graph2(tree: Tree, target: Author, *, configuration: Dict[str, Any]):
-    graph = None
+def generate_graph(tree: Tree, target: Author, *, configuration: Dict[str, Any]):
     if configuration['graph_type'] == 'directed':
         graph = Digraph("Authors")
     else:
         graph = Graph("Authors")
+
     img_path = configuration["image_path"]
     graph_path = configuration["graph_path"]
     graph_name = configuration["graph_name"]
@@ -55,7 +55,7 @@ def generate_graph2(tree: Tree, target: Author, *, configuration: Dict[str, Any]
                             <TD  bgcolor="white" border="0">{additional_node.get_short_name(10)}</TD>
                         </TR>
                         </TABLE>>""",
-                        'image': f'{img_path+additional_node.id}.png',
+                        'image': f'{img_path + additional_node.id}.png',
                         'shape': 'plaintext',
                         'labelloc': 'b',
                         'fixedsize': 'true',
@@ -78,143 +78,7 @@ def generate_graph2(tree: Tree, target: Author, *, configuration: Dict[str, Any]
                     <TD  bgcolor="white" border="0">{node.get_short_name(10)}</TD>
                 </TR>
                 </TABLE>>""",
-                'image': f'{img_path+node.id}.png',
-                'shape': 'plaintext',
-                'labelloc': 'b',
-                'fixedsize': 'true',
-                'width': '1',
-                'height': '1',
-                'imagescale': 'true',
-                'fontsize': '8',
-            }
-        )
-
-        if previous_node is not None:
-            graph.edge(previous_node.id, node.id, _attributes={'color': main_branch_color})
-        previous_node = node
-
-    graph.node('legend', '', {
-        'label': f"""<
-            <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
-             <TR>
-              <TD COLSPAN="2"><B>Legend</B></TD>
-             </TR>
-             <TR>
-              <TD>Main Branch</TD>
-              <TD BGCOLOR="{main_branch_color}"></TD>
-             </TR>
-             <TR>
-              <TD>Additional Branch</TD>
-              <TD BGCOLOR="{additional_branch_color}"></TD>
-             </TR>
-            </TABLE>
-           >""",
-        'shape': 'plaintext',
-        'fontsize': '8'
-    })
-
-    graph.render(graph_name, graph_path, cleanup=True)
-
-
-
-def generate_graph(tree: Tree, target: Author, configuration: Dict[str, Any]):
-    graph = None
-
-    try:
-        if configuration['graph_type'] == 'directed':
-            graph = Digraph("Authors")
-        else:
-            graph = Graph("Authors")
-    except KeyError:
-        raise ValueError("Missing requirement configuration: 'graph type'")
-
-    try:
-        img_path = configuration["image_path"]
-    except KeyError:
-        raise ValueError("Missing requirement configuration: 'image_path'")
-
-    try:
-        graph_path = configuration["graph_path"]
-    except KeyError:
-        raise ValueError("Missing requirement configuration: 'graph_path'")
-
-    try:
-        graph_name = configuration["graph_name"]
-    except KeyError:
-        graph_name = "Authors"
-
-
-    try:
-        graph.format = configuration['format']
-    except KeyError:
-        ...
-
-    additional_nodes_limit = 0
-    try:
-        additional_nodes_limit = configuration['additional_nodes']
-    except KeyError:
-        ...
-
-    main_branch_color = "BLACK"
-    try:
-        main_branch_color = configuration['main_branch_color']
-    except KeyError:
-        ...
-
-    additional_branch_color = "BLACK"
-    try:
-        additional_branch_color = configuration['additional_branch_color']
-    except KeyError:
-        ...
-
-    main_branch = tree.tree[target.id]
-    previous_node = None
-    nodes = []
-
-    for node in main_branch:
-        Fetch.fetch_image(node, img_path)
-        additional_nodes = 0
-        nodes.append(node.id)
-
-        for additional_node in node.coauthors:
-            if additional_node.id not in nodes and additional_nodes <= additional_nodes_limit and additional_node not in main_branch:
-                if isinstance(additional_node, AuthorCredentials):
-                    additional_node = additional_node._source.fetch_by_credentials(additional_node)
-                Fetch.fetch_image(additional_node, img_path)
-                additional_nodes += 1
-                graph.node(
-                    str(additional_node.id),
-                    '',
-                    {
-                        'label': f"""<<TABLE border="0">
-                        <TR>
-                            <TD  bgcolor="white" border="0">{additional_node.get_short_name(10)}</TD>
-                        </TR>
-                        </TABLE>>""",
-                        'image': f'{img_path+additional_node.id}.png',
-                        'shape': 'plaintext',
-                        'labelloc': 'b',
-                        'fixedsize': 'true',
-                        'width': '1',
-                        'height': '1',
-                        'imagescale': 'true',
-                        'fontsize': '8',
-                    }
-                )
-                graph.edge(node.id, additional_node.id, _attributes={'color': additional_branch_color})
-            else:
-                break
-
-        graph.node(
-            str(node.id),
-            '',
-            {
-                'label': f"""<<TABLE border="0">
-                <TR>
-                    <TD  bgcolor="white" border="0">{node.get_short_name(10)}</TD>
-                </TR>
-                </TABLE>>""",
-                'image': f'{img_path+node.id}.png',
+                'image': f'{img_path + node.id}.png',
                 'shape': 'plaintext',
                 'labelloc': 'b',
                 'fixedsize': 'true',
